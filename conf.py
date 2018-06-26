32f59be61d50a7a22b7e3078e27fb2384cfbabcf
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2018-06-26 16:52:16 +0200

    Update manual version to 4.1
    Official 4.1 release is tomorrow :)
    
    Ref T8652

--- conf.py
+++ conf.py
@@ -42,9 +42,9 @@ except subprocess.CalledProcessError as exc:
     gitcommithash = release
 
 # The short X.Y version
-version = '4.0'
+version = '4.1'
 # The full version, including alpha/beta/rc tags
-release = '4.0.3'
+release = '4.1'
 
 
 # -- General configuration ---------------------------------------------------
